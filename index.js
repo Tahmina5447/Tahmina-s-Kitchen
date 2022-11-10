782c960968917c0d5fe10206d0ca36d9645ec99d
Tahmina <tahminakhatun5447@gmail.com>
2022-11-10 11:18:16 +0600

    add update function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const port =process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt=require('jsonwebtoken')
 require('dotenv').config();
 
 app.use(cors());
@@ -14,6 +15,36 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.01makvu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 // console.log(uri)
+
+
+
+function verifyJWT(req,res,next){
+  const authHeader=req.headers.authorization;
+  // console.log(authHeader)
+  if(!authHeader){
+    return res.status(401).send({message:'unauthorized access'})
+  }
+  const token=authHeader.split(' ')[1];
+  // console.log(token)
+  jwt.verify(token,process.env.TOKEN,function(err,decoded){
+    if(err){
+      return res.status(401).send({message:'unauthorized access'})
+    }
+    // console.log(decoded)
+    req.decoded=decoded;
+    next();
+  })
+
+
+}
+
+
+
+
+
+
+
+
 
 
 async function run(){
@@ -35,9 +66,18 @@ async function run(){
       const query={};
       const cursor=itemsCollection.find(query);
       const allItems=await cursor.toArray();
-      // console.log(allItems)
+          // console.log(allItems)
       res.send(allItems);
     })
+
+    // for jwt token
+    app.post('/jwt',(req,res)=>{
+      const user=req.body;
+            // console.log(user)
+      const token=jwt.sign(user,process.env.TOKEN);
+      res.send({token});
+    })
+
 
     // add item by customer
     app.post('/allItems',async(req,res)=>{
@@ -58,11 +98,13 @@ async function run(){
     app.post('/review',async(req,res)=>{
       const review=req.body;
       const result=await reviewCollection.insertOne(review);
+      console.log(result)
       res.send(result);
     })
 
     // get review by id
     app.get('/review',async(req,res)=>{
+      
       let  query={};
       console.log(req.query.email)
       if(req.query.item){
@@ -72,12 +114,16 @@ async function run(){
       }
       
       const cursor=reviewCollection.find(query);
-      const review=await cursor.toArray();
+      const review=await cursor.sort({_id:-1}).toArray();
       res.send(review);
     })
 
     // get review by user
-    app.get('/userReview',async(req,res)=>{
+    app.get('/userReview',verifyJWT,async(req,res)=>{
+      const decoded=req.decoded;
+      if(decoded.email !== req.query.email){
+        return res.status(403).send({message:'unauthorized access'})
+      }
       let query={};
       if(req.query.email){
         query={
@@ -87,6 +133,31 @@ async function run(){
       const cursor=reviewCollection.find(query);
       const reviews=await cursor.toArray();
       res.send(reviews);
+    })
+
+
+    // delete review
+    app.delete('/userReview/:id',async(req,res)=>{
+      const id= req.params.id;
+      const query={_id:ObjectId(id)}
+      const result=await reviewCollection.deleteOne(query);
+      console.log(result)
+      res.send(result);
+    })
+
+
+    // update review
+    app.patch('/userReview/:id',async(req,res)=>{
+      const id=req.params.id;
+      const status=req.body.status;
+      const query={_id:ObjectId(id)}
+      const updatedReview={
+        $set:{
+          message:status
+        }
+      }
+      const result = await reviewCollection.updateOne(query,updatedReview);
+      res.send(result);
     })
 
   }finally{
